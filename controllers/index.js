600578569c34b1fca766a12087bba68e294d0c27
pelpax1 <s.lillonavia@gmail.com>
2016-09-09 13:26:05 -0300

    jsonx

--- controllers/index.js
+++ controllers/index.js
@@ -16,8 +16,9 @@ module.exports = function (router) {
     router.post(´/´, function (req, res){
         
         var x = req.body;
-        res.send('<code><pre>' + req.body + '</pre></code>');
-        res.status(200).end();
+        //res.send('<code><pre>' + req.body + '</pre></code>');
+        //res.status(200).end();
+        res.status(200).jason(x).end();
     });
 
 };
