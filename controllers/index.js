861d5f8360feb18012252a23b55e9da069343846
pelpax1 <s.lillonavia@gmail.com>
2016-09-09 13:13:45 -0300

    index.js

--- controllers/index.js
+++ controllers/index.js
@@ -13,4 +13,11 @@ module.exports = function (router) {
         
     });
 
+    router.post(´/´, function (req, res)){
+        
+        var x = req.body;
+        res.send('<code><pre>' + req.body + '</pre></code>');
+        res.status(200).end;
+    });
+
 };
